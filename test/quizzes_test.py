5ab1d4461a8a609495943a3167a575975ab3399c
Amir Ghaemi <51731855+amirmghaemi@users.noreply.github.com>
2024-04-21 15:04:49 -0400

    Update quizzes_test.py
    Adding the first unit test

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -8,12 +8,18 @@ class QuizzesTest(unittest.TestCase):
         # Run tests on non-production data
         self.ctrl = QuizzesController('quizzes_test.py')
         
-    def test_expose_failure_01(self):
+ def test_expose_failure_01(self):
         """
-        Implement this function and two more that
-        execute the code and make it fail.
+        We load a JSON file that is empty
+
+        The error occurs in the app/utils/data_loader.py, line 13 when loading the data:
+            return json.load(fin)
+
+        json.decoder.JSONDecodeError: Expecting property name enclosed in double quotes: line 1 column 2 (char 1)
         """
-        self.assertTrue(True, 'Example assertion.')
+        _ = QuizzesController("bad_data.json")
+        assert _ is QuizzesController
+         self.assertIsNone(newly_added_question, "Test should fail as no data exists in the JSON")
         
 
 if __name__ == '__main__':
