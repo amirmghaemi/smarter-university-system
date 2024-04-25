d539b3d7d766daba4f0c44e43913e9dbcccbb6c8
Jin Liu <jin@Jins-MacBook-Pro.local>
2024-04-25 18:02:22 -0400

    added test_expose_failure_03

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import unittest
 
 from app.controllers.quizzes_controller import QuizzesController
@@ -20,7 +21,25 @@ class QuizzesTest(unittest.TestCase):
         _ = QuizzesController("bad_data.json")
         assert _ is QuizzesController
          self.assertIsNone(newly_added_question, "Test should fail as no data exists in the JSON")
-        
+    
+    def test_expose_failure_03(self):
+        """
+        Testing the ability to create a quiz with a NoneType data and causing a crash
+        """
+        test_available_date = datetime(2024, 4, 23, 12, 0, 0)
+        test_due_date = datetime(2024, 4, 24, 12, 0, 0)
+        test_quiz_id = self.ctrl.add_quiz(None, 'test', test_available_date, test_due_date)
+        test_quiz = self.ctrl.get_quiz_by_id(test_quiz_id)
+        self.assertIsNotNone(test_quiz, 'Get None quiz.')
+
+        '''
+        crash info:
+        File "smarter-university-system/./app/controllers/quizzes_controller.py", line 63, in add_quiz
+            quiz_id = utils.generate_id(title + updated_date.isoformat())
+        TypeError: unsupported operand type(s) for +: 'NoneType' and 'str'
+        '''
+
+
 
 if __name__ == '__main__':
     unittest.main()
